e9baa16110ef8339326455add7c65d612503c777
Sergey-Petukhov <46074388+Sergey-Petukhov@users.noreply.github.com>
2019-01-27 21:35:10 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,31 @@
+MONGO_URI = "mongodb://phonebook_user:phonebook_password@127.0.0.1:27017/phonebook"
+
+RESOURCE_METHODS = ['GET', 'POST']
+ITEM_METHODS = ['GET']
+X_DOMAINS = '*'
+VALIDATION_ERROR_STATUS = 200
+CACHE_CONTROL = 'max-age=5'
+CACHE_EXPIRES = 5
+
+DOMAIN = {
+    'contacts': {
+        'schema': {
+            'name': {
+                'type': 'string',
+		'regex': '^\s*[а-яА-ЯёЁa-zA-Z]+[а-яА-ЯёЁa-zA-Z\s]*$',
+                'minlength': 1,
+                'maxlength': 50,
+                'required': True,
+                'unique': True
+            },
+            'phone': {
+                'type': 'string',
+		'regex': '^\s*[\+\(]?[\d]{1,5}[\-\s\/\)]?[\(\s]?[\d]{0,5}[\)]?[\-\s\/\.]?[\d]{0,5}[\-\s\/\.]?[\d]{0,5}[\-\s\/\.]?[\d]{0,5}\s*$',
+                'minlength': 1,
+                'maxlength': 50,
+                'required': True,
+		'unique': True
+            }
+        }
+    }
+}
